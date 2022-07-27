3fc2e794927df74b2454b6777c0afd2df551553f
Behnam S.A <behnamsa96@outlook.com>
2022-07-28 04:03:06 +0430

    DoneDate unit tests are added

---
+++ src/Domain.UnitTest/TodoItems/TodoItemTests.cs
@@ -0,0 +1,31 @@
+﻿using Domain.TodoItems;
+using FluentAssertions;
+using NUnit.Framework;
+
+namespace Domain.UnitTest.TodoItems
+{
+    public class TodoItemTests
+    {
+        [Test]
+        public void WhenTodoItemGetsDoneDateShouldBeUpdated()
+        {
+            var todoItem = new TodoItem();
+
+            todoItem.MakeItDone();
+
+            todoItem.DoneDate.Should().Be(DateTime.UtcNow.Date);
+        }
+
+        [Test]
+        public void WhenTodoItemGetsUndoneDateShouldBe()
+        {
+            var todoItem = new TodoItem();
+
+            todoItem.MakeItDone();
+
+            todoItem.MakeItUndone();
+
+            todoItem.DoneDate.Should().BeNull();
+        }
+    }
+}
---
+++ src/Domain/TodoItems/TodoItem.cs
@@ -0,0 +1,20 @@
+﻿namespace Domain.TodoItems
+{
+    public sealed class TodoItem
+    {
+        public bool IsDone { get; private set; }
+        public DateTime? DoneDate { get; private set; }
+
+        public void MakeItDone()
+        {
+            IsDone = true;
+            DoneDate = DateTime.UtcNow.Date;
+        }
+
+        public void MakeItUndone()
+        {
+            IsDone = false;
+            DoneDate = null;
+        }
+    }
+}
