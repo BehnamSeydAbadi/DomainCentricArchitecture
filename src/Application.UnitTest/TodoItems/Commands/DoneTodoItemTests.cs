1c2935c7a63b10fb21c707ace38fcab21d0c8c17
Behnam S.A <behnamsa96@outlook.com>
2022-07-29 17:33:18 +0430

    DoneDateShouldBeCorrectDateOfWhenTodoItemGetsDoneAsync test passed

--- src/Application.UnitTest/TodoItems/Commands/DoneTodoItemTests.cs
+++ src/Application.UnitTest/TodoItems/Commands/DoneTodoItemTests.cs
@@ -1,9 +1,34 @@
-﻿using Application.UnitTest.Common;
+﻿using Application.TodoItems.Commands;
+using Application.UnitTest.Common;
+using Domain.TodoItems;
+using FluentAssertions;
+using NUnit.Framework;
 
 namespace Application.UnitTest.TodoItems.Commands
 {
     public class DoneTodoItemTests : BaseTest
     {
+        private DoneTodoItemCommandHandler _commandHandler;
 
+        [SetUp]
+        public void Setup() => _commandHandler = new DoneTodoItemCommandHandler(_todoContext);
+
+        [Test]
+        public async Task DoneDateShouldBeCorrectDateOfWhenTodoItemGetsDoneAsync()
+        {
+            //Arange
+            var todoItem = new TodoItem(RandomTodoItemTitle);
+
+            await _todoContext.TodoItems.AddAsync(todoItem);
+            await _todoContext.SaveChangesAsync();
+
+
+            //Act
+            await _commandHandler.HandleAsync(new DoneTodoItemCommand(todoItem.Id));
+
+
+            //Arange
+            todoItem.DoneDate.Should().NotBeNull().And.Be(DateTime.Now.Date);
+        }
     }
 }
