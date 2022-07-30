027d45e721915c3ec91a549e38f581343f75eacc
Behnam S.A <behnamsa96@outlook.com>
2022-07-31 00:11:41 +0430

    TodoApplicationFactory added

---
+++ src/Application.UnitTest/Common/TodoApplicationFactory.cs
@@ -0,0 +1,26 @@
+﻿using Infrastructure;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.VisualStudio.TestPlatform.TestHost;
+
+namespace Application.IntegrationTest.Common
+{
+    internal sealed class TodoApplicationFactory : WebApplicationFactory<Program>
+    {
+        protected override void ConfigureWebHost(IWebHostBuilder builder)
+        {
+            var todoContextOptionsBuilder = new DbContextOptionsBuilder<TodoContext>()
+                .UseInMemoryDatabase(databaseName: "TodoDb")
+                .Options;
+
+            builder.ConfigureServices((WebHostBuilderContext builder, IServiceCollection services) =>
+            {
+                services.AddDbContext<TodoContext>(
+                    (serviceProvider, dbContextOptionsBuilder)
+                        => dbContextOptionsBuilder.UseInMemoryDatabase(databaseName: "TodoDb"));
+            });
+        }
+    }
+}
