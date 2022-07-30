52620534b6295c86299ec0aa99ea72430e546f2e
Behnam S.A <behnamsa96@outlook.com>
2022-07-30 23:31:09 +0430

    TodayController added

---
+++ src/Presentation/TodoItems/TodayController.cs
@@ -0,0 +1,20 @@
+﻿using Application.Common;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Presentation.TodoItems
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TodayController : ControllerBase
+    {
+        private readonly IQueryHandler<TodoItem> _queryHandler;
+
+        public TodayController(IQueryHandler<TodoItem> queryHandler) => _queryHandler = queryHandler;
+
+        [HttpGet]
+        public async Task<IActionResult> Get()
+        {
+            
+        }
+    }
+}
