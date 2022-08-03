9986c46474878b0968bd3c98067e3a3063c51409
Behnam S.A <behnamsa96@outlook.com>
2022-08-04 03:34:17 +0430

    DoneTodoItem Api added

---
+++ src/Presentation/TodoItems/Commands/DoneTodoItem/DoneTodoItemController.cs
@@ -0,0 +1,23 @@
+﻿using Application.Common;
+using Application.TodoItems.Commands.DoneTodoItem;
+using Microsoft.AspNetCore.Mvc;
+using Presentation.Common;
+
+namespace Presentation.TodoItems.Commands.DoneTodoItem
+{
+    public class DoneTodoItemController : BaseApiController
+    {
+        private readonly ICommandHandler<DoneTodoItemCommand> _commandHandler;
+
+        public DoneTodoItemController(ICommandHandler<DoneTodoItemCommand> commandHandler)
+            => _commandHandler = commandHandler;
+
+        [HttpPut]
+        public async Task<IActionResult> Put(int id)
+        {
+            await _commandHandler.HandleAsync(new DoneTodoItemCommand(id));
+
+            return Ok();
+        }
+    }
+}
