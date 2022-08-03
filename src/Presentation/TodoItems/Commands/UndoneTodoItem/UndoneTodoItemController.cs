d48201f21dc25c487fed4af6ed92477a61a2db38
Behnam S.A <behnamsa96@outlook.com>
2022-08-04 03:36:15 +0430

    UndoneTodoItem api added

---
+++ src/Presentation/TodoItems/Commands/UndoneTodoItem/UndoneTodoItemController.cs
@@ -0,0 +1,23 @@
+﻿using Application.Common;
+using Application.TodoItems.Commands.UndoneTodoItem;
+using Microsoft.AspNetCore.Mvc;
+using Presentation.Common;
+
+namespace Presentation.TodoItems.Commands.UndoneTodoItem
+{
+    public class UndoneTodoItemController : BaseApiController
+    {
+        private readonly ICommandHandler<UndoneTodoItemCommand> _commandHandler;
+
+        public UndoneTodoItemController(ICommandHandler<UndoneTodoItemCommand> commandHandler)
+            => _commandHandler = commandHandler;
+
+        [HttpPut]
+        public async Task<IActionResult> Put(int id)
+        {
+            await _commandHandler.HandleAsync(new UndoneTodoItemCommand(id));
+
+            return Ok();
+        }
+    }
+}
