08a18b261fe935a69e6e06010ce67cbce435670c
Behnam S.A <behnamsa96@outlook.com>
2022-08-04 03:31:34 +0430

    DeleteTodoItem Api added

---
+++ src/Presentation/TodoItems/Commands/DeleteTodoItem/DeleteTodoItemController.cs
@@ -0,0 +1,23 @@
+﻿using Application.Common;
+using Application.TodoItems.Commands.DeleteTodoItem;
+using Microsoft.AspNetCore.Mvc;
+using Presentation.Common;
+
+namespace Presentation.TodoItems.Commands.DeleteTodoItem
+{
+    public class DeleteTodoItemController : BaseApiController
+    {
+        private readonly ICommandHandler<DeleteTodoItemCommand> _commandHandler;
+
+        public DeleteTodoItemController(ICommandHandler<DeleteTodoItemCommand> commandHandler)
+            => _commandHandler = commandHandler;
+
+        [HttpDelete]
+        public async Task<IActionResult> Delete(int id)
+        {
+            await _commandHandler.HandleAsync(new DeleteTodoItemCommand(id));
+
+            return Ok();
+        }
+    }
+}
